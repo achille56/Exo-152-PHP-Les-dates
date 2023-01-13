1089584bb7013f38fe0e1d46823fc1e491bfb9cc
achille56 <leclercqjm59@gmail.com>
2023-01-13 13:19:30 +0100

    exo 152

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 
 // Premiere ligne
+$today = date("D");
+    echo $today;
 
+echo "<br><br>";
 
+echo date("Y.m.d", mktime(0, 0,0,12,10,2018));
+
+echo "<br><br>";
+
+echo date("h:i:s", mktime(11,35,07));
 
 
 
